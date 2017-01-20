429b8f2c83cbd9ab43eea5cdefc204b80f5cffe6
EllisBoydRedding <demircia@itu.edu.tr>
2017-01-21 00:35:40 +0300

    Added  functionality passing from intro screen to selection screen

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -7,10 +7,17 @@ namespace Maca
 {
     public class GUIManager : Singleton<GUIManager>
     {
-
         public GameObject Intro;                     
         public GameObject Selections;                       
         public GameObject Settings;
         public GameObject InGame;
+
+        public void GoSelectionScreen()
+        {
+            Intro.SetActive(false);
+            Selections.SetActive(true);
+            //Settings.SetActive(false);
+            //InGame.SetActive(false);
+        }
     }
 }
