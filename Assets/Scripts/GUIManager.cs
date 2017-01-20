b857b01e054a769ccb95691d0cefa94d3890e042
EllisBoydRedding <demircia@itu.edu.tr>
2017-01-20 21:29:30 +0300

    Created main folders, scripts, icons and intro screen

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Maca
+{
+    public class GUIManager : Singleton<GUIManager>
+    {
+
+        public GameObject Intro;                     
+        public GameObject Selections;                       
+        public GameObject Settings;
+        public GameObject InGame;
+    }
+}
